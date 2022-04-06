d13af3a824aea666e27f0f2ca81e49786b7a8a20
wa <wa@wa-MS-7C91>
2022-04-06 04:27:42 -0400

    trying to improve code efficiency, migrate to source format 2 cpp, 1 h

--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,11 @@
 #include "raylib.h"
 #include "crakanoid.h"
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <iterator>
-#include <cstring>
-#include <experimental/filesystem>
 
 using namespace crakanoid;
-using std::cout; using std::cin;
-using std::endl; using std::string;
-using std::vector;
-using std::experimental::filesystem::directory_iterator;
-
-const string PATH = "../resources/";
-const auto DIR = directory_iterator(PATH);
-
-void randomSong(vector<string>& songPlayedOnce, vector<string>& songPaths)
-{
-	songPaths = {};
-	for(const auto & file : DIR)
-	{
-		cout << file.path() << endl;
-		auto teststr = std::find(songPlayedOnce.begin(), songPlayedOnce.end(), file.path());
-		if(teststr != std::end(songPlayedOnce))
-		{
-			cout << "In there!" << endl;
-		}
-		else
-		{
-			songPaths.push_back(file.path());
-		}
-//		if(std::find(songPlayedOnce.begin(), songPlayedOnce.end(), file.path()))
-//		{
-//			songPaths.push_back(file.path());
-//		}
-	}
-}
 
 int main(void)
 {
 	srand(time(0));
-
-	vector<string> songPlayedOnce{};
-	vector<string> songPaths{};
-//	randomSong(&songPlayedOnce, &songPaths);
-//	vector<string> songPaths{};
-
-	for(const auto & file : DIR)
-	{
-		cout << file.path() << endl;
-		auto teststr = std::find(songPlayedOnce.begin(), songPlayedOnce.end(), file.path());
-		if(teststr != std::end(songPlayedOnce))
-		{
-			cout << "In there!" << endl;
-		}
-		else
-		{
-			songPaths.push_back(file.path());
-		}
-//		if(std::find(songPlayedOnce.begin(), songPlayedOnce.end(), file.path()))
-//		{
-//			songPaths.push_back(file.path());
-//		}
-	}
-
-	cout << songPaths.size() << endl;
-	cout << songPlayedOnce.size() << endl;
-
-	vector<string> t = { "trtr" , "sdfsdf" };
-
-	int randNumber = rand() % songPaths.size();
-	cout << "rand: " << randNumber << endl;
-	cout << "song path: " << songPaths[1] << endl;
-
-//	const int randomSong = rand() % songPaths.size();
-
-	string songPlayPath = songPaths[randNumber];
-	songPlayedOnce.push_back(songPaths[randNumber]);
-	cout << songPlayedOnce.size() << endl;
-
-	cout << "song played: " << songPlayPath << endl;
-
 
     // Initialization
     //--------------------------------------------------------------------------------------
@@ -91,19 +15,15 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Crakanoid");
 
     InitAudioDevice();              // Initialize audio device
-
-
     SetAudioStreamBufferSizeDefault(MAX_SAMPLES_PER_UPDATE);
 
+    //convert string to char* and play the song
+	vector<string> songPlayedOnce{};
+    string songPlayPath = randomSong(songPlayedOnce);
     int n = songPlayPath.length();
-
-    // declaring character array
     char char_array[n + 1];
-
-    // copying the contents of the
-    // string to char array
     strcpy(char_array, songPlayPath.c_str());
-
+    cout << "char: " << char_array << endl;
     Music songPlayed = LoadMusicStream(char_array);
     PlayMusicStream(songPlayed);
 
