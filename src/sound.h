ddd1acb0936a14b4ad0c469927b1a89413de4be7
wa <wa@wa-MS-7C91>
2022-04-08 23:25:06 -0400

    removed experimental/filesystem

--- src/sound.h
+++ src/sound.h
@@ -4,7 +4,6 @@
 #include <algorithm>
 #include <iterator>
 #include <cstring>
-#include <experimental/filesystem>
 #include <chrono>
 #include <array>
 #include <random>
@@ -13,7 +12,6 @@
 using std::cout; using std::cin;
 using std::endl; using std::string;
 using std::vector;
-using std::experimental::filesystem::directory_iterator;
 
 #define MAX_SAMPLES               512
 #define MAX_SAMPLES_PER_UPDATE   4096
